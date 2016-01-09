544bf0cdfe2b47d86c814f3657726310ba2eb2d1
peterling <shap50@gmail.com>
2016-01-09 02:08:05 +0100

    "some garbage collection" and stop process (still todo)

--- firstImport.go
+++ firstImport.go
@@ -27,7 +27,6 @@ type xmlConfig struct {
 
 
 var	v = xmlConfig{}
-var überwachteProzesse = make(map[int]string)
 var laufendeProzesse = make([] string,0)
 var procSlice = make([]*exec.Cmd, 0)
 var stopperProcSlice = make([]*exec.Cmd, 0)
@@ -84,34 +83,6 @@ func killingProcessSoftly(pid int){
 	p.Signal(os.Interrupt)
 	}
 }
-//bekommt die PID und versucht den Prozess über seinen Bash-Stop-Befehl zu beenden
-func programmBashStop(pid int){
-	//wenn in XML eintrag geändert wurde, soll der bereits laufende prozess sich dennoch mit seinem ursprünglichen bash-stop-befehl beenden lassen
-	//hierzu wird aus der ProzesseMap der hinterlegte Stop-Befehl (mitsamt Programmname) ausgelesen
-	stopBefehlUndName := überwachteProzesse[pid]
-    //fmt.Println("stopBefehlUndName: ", stopBefehlUndName)
-	//Programmname abgetrennt, wir brauchen hier nur den Stop-Befehl
-	keySplitStopBefehl:=strings.Split(stopBefehlUndName,",")
-//var befehlKomplett string = 
-//fmt.Println(keySplitStopBefehl[0])
-//Der Stop-Befehl enthält u.U. Parameter, hierzu erneut eine Aufsplittung (des Stop-Befehls)
-befehlSplit :=strings.Split(keySplitStopBefehl[0]," ")	
-cmd := exec.Command(befehlSplit[0], befehlSplit[1:]...)
-//Ausführung des Stop-Befehls
-cmd.Start()
-stdout, _ := cmd.StdoutPipe()
-go io.Copy(os.Stdout, stdout)
-/*_, prs := m[pid]
-if prs{
-	fmt.Println("Process still running!")
-}*/
-
-}
-
-/*func programmStarter(asd *exec.Cmd)*os.Process{
-	asd.Start()
-	return asd.Process
-}*/
 
 
 func programmStart(programmNr int){
@@ -135,8 +106,6 @@ runtime.Gosched()
 	cmd.Run()
 	//cmd.Start()
 	//cmd.Wait()
-
-überwachteProzesse[cmd.Process.Pid] = v.ProgrammStopListe[programmNr]+", "+v.ProgrammNamenListe[programmNr]
 
 	//p,_:=os.FindProcess(cmd.Process.Pid)
 		fmt.Printf(v.ProgrammNamenListe[programmNr]+" wurde gestartet, ") //auf diese ausgabe ist kein verlass (kein real time, erst nach beendigung der funktion)
@@ -186,7 +155,7 @@ func watchFile() error {
 func main() {
 	//Programminitialisierung
 	xmlReadIn()			//XML-Datei einlesen lassen
-//	go watchFile()		//Veränderungen an der XML erkennen, ggfs. neu einlesen
+	go watchFile()		//Veränderungen an der XML erkennen, ggfs. neu einlesen
 	go programFlow()	//Aufrufe in Goroutine starten
 	 fmt.Scanln()		//Programm weiterlaufen lassen ohne Ende
 	}
@@ -195,7 +164,7 @@ func cleanProcSlice(progra int){
 	mutexProcSlice.Lock()
 	if procSlice[progra].ProcessState.Exited(){
 	fmt.Println("ASDLKGLDFLKDFLGK")
-	
+//	procSlice[progra].ProcessState.String() ggfs. hiermit, vgl. startprocess
 	}
 	mutexProcSlice.Unlock()
 	runtime.Gosched()
@@ -213,6 +182,7 @@ befehlSplit :=strings.Split(nameAndStopcmd,", ")
 
 	
 func programmStop(progra int){
+	//defer if processstate war nicht exit... dann .kill ?
 	var nameAndStopcmd string = procSliceNameAndStopcmd[progra]
 	befehlKomplett :=strings.Split(nameAndStopcmd,", ")
 	befehlSplit :=strings.Split(befehlKomplett[1]," ")	
@@ -227,11 +197,13 @@ func programmStop(progra int){
 	stopperProcSlice = append(stopperProcSlice, cmd) //Befehl zum Stoppen könnte auch hängen, Name wird aber nicht gespeichert (overKill)
 	mutexStopperProcSlice.Unlock()
 	runtime.Gosched()
-	//cmd.Run()
+	cmd.Run()
 	//cmd.Wait()
-	cmd.Start()
+	//cmd.Start()
 	fmt.Println("Der für das Programm "+befehlKomplett[0]+" mit PID "+ strconv.Itoa(procSlice[progra].Process.Pid)+" ursprünglich hinterlegte Stop-Befehl wurde ausgeführt")
-}
+fmt.Println(stopperProcSlice)
+fmt.Println(stopperProcSlice[0].ProcessState.String())
+}//TODO Stoppbefehlliste bereinigen ?! wie bei startprogrammliste auch gemacht...
 
 
 func programFlow(){
@@ -261,6 +233,14 @@ func programFlow(){
 	//	programmStop(1)
 		processeAufKonsoleAusgeben()
 			processeAufKonsoleAusgeben()
+				
+				
+			go	programmStop(0)
+			go	programmStop(0)
+			programmStop(0)
+			
+			
+			
 		//	fmt.Println("Folgende Prozesse laufen gerade: ")
 		//	fmt.Println(procSlice)
 		//	fmt.Println(procSliceNameAndStopcmd)
@@ -270,6 +250,8 @@ func programFlow(){
 }
 func processeAufKonsoleAusgeben(){
 var procSliceNotExited = make([]string, 0)
+mutexProcSlice.Lock()
+mutexProcSliceNameAndStopcmd.Lock()
 for k:= range procSlice {
 	asd := procSlice[k].ProcessState.String()
 //	asd:=procState.String()
@@ -285,6 +267,9 @@ for k:= range procSlice {
 //procSlice = procSliceNew[0:]
 //copy(procSlice,procSliceNew)
 fmt.Println(procSlice)
+mutexProcSlice.Unlock()
+mutexProcSliceNameAndStopcmd.Unlock()
+runtime.Gosched()
 fmt.Println(procSliceNotExited) //diesen für die website zur anzeige der laufenden prozesse verwenden
 }
 
@@ -367,19 +352,6 @@ for k:=range procSlice{
 	}
 	mutexProcSlice.Unlock()
 		runtime.Gosched()
-	}*/
-/*	mutexProcSlice.Lock()
-	procListe := procSlice
-	mutexProcSlice.Unlock()
-	runtime.Gosched()
-*/	/*
-	for {
-		if procSlice != nil {
-	for k:= range procSlice{
-		if procSlice[k].ProcessState.Exited(){
-			fmt.Println("Exited!")
-		}
-	}
 	}
 	time.Sleep(2 * time.Second)
 	}
@@ -406,122 +378,17 @@ for k:=range procSlice{
   //  for k := range überwachteProzesse {
     //    keys = append(keys, k)
    // }
-//	fmt.Println(keys)
-	//go killingProcessHard(keys[0])
-//	go killingProcessHard(keys[1])
-//	go killingProcessHard(keys[2])
-/*	for t := range keys {
-		fmt.Println(keys[t])
-		asdd,_ :=os.FindProcess(keys[t])
-		asds,_:=asdd.Wait()
-		if (asds != nil) {fmt.Println("WAITING")}
-	}
-*///}
-	
 
-	/*
-		//	programmBashStop(keys[1])
-		
-		//	killingProcessSoftly(keys[1])
-			fmt.Println()
-			fmt.Println(überwachteProzesse)
-		fmt.Println(killingProcessHard(keys[1]))
-			fmt.Println(killingProcessHard(keys[1]))
-			fmt.Println("ENDE")
-		
-	//		processRunning(keys[1])
-			fmt.Println(keys[1])
-		
-		//	programmBashStop(programmStart(1))
-		
-		
-*/
-			
-			
 
-			//if processRunning(pid){fmt.Println("laeuft noch!!!")}
-	//p,_:=os.FindProcess(programmStart(auswahl))
-	//fmt.Printf(programmListeName[auswahl]+" wurde gestartet,")
-	//fmt.Printf("PID lautet: %d",p.Pid)
-	
-	//fmt.Println(<-runningProc)
-	//fmt.Println(<-runningProc)
-	//fmt.Println(<-runningProc)
-//fmt.Println(überwachteProzesse)
-	//fmt.Print(programmStart(1))
-	//programmStart(1)
-	//go programmStart(1)
-	//programmStart(2)
-//programmStart(2)
-	//Irgendwie für jede Applikation einen exec.Command erstellen... nur wie ?
-
-	
-	//cmd_peter := exec.Command("ping", "8.8.8.8")
-	// Create stdout, stderr streams of type io.Reader
-	//fmt.Println(programmListeStart[0])
-    //Standard-Ausgabe und -Fehler umleiten - momentan noch auf die Konsole...
-
-	/*ch := make(chan string, 20)
-	ch <- "hallo"
-	ch <- "palom"
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
-*/
 	
 //	stdout_peter, err_peter := cmd_peter.StdoutPipe()
-  //  checkError(err_peter)
-	
-	/*	stdin, err := cmd_peter.StdinPipe()
-if err != nil {
-    fmt.Println(err)
-}
-defer stdin.Close()*/
-//io.WriteString(stdin, "dir\n")
-//io.WriteString(stdin, "dir\n")
-//io.WriteString(stdin, "exit\n")
+
 //signal.Notify(stdin, os.Interrupt)
 	
 	
-	//Standard-Eingabe umleiten. Evtl. garnicht notwendig, wenn wir mit SIGINT arbeiten können...
-	//stdin_peter, err_peter := cmd_peter.StdinPipe()
-	//checkError((err_peter))
-   // stderr_peter, err_peter := cmd_peter.StderrPipe()
-    //checkError(err_peter)
-
-    //Die oben angelegten exec.Commands ausführen.
-  //  err = cmd.Start()
-   // checkError(err)
-//	err_peter = cmd_peter.Start()
-//	checkError(err_peter)
-
-    // main() darf erst beendet werden, wenn unsere Commands fertig sind...
- //   defer cmd.Wait()  // blockt nicht, läuft also weiter
-//	defer cmd_peter.Wait()
-	
-    // nicht-blockende echo-Ausgabe, momentan zum Terminal... (go ...)
- //   go io.Copy(os.Stdout, stdout)
-   // go io.Copy(os.Stderr, stderr)
-  //  go io.Copy(os.Stdout, stdout_peter)
-//	go io.Copy(stdin_peter, os.Stdin)
-	//go io.Copy(ioutil.ReadAll(os.Stdin), stdin_peter)
-
-//Auslesen der PIDs zum Steuern der Prozesse... muss noch erforscht werden
-//fmt.Println(cmd_peter.Process.Pid)
-//fmt.Println(cmd_peter.Process.Pid)
-//fmt.Println(cmd.Process.Pid)
-//fmt.Println(os.Getpid()) //Achtung, PID der Hauptanwendung!
 
 //unter Windows funktioniert nur Kill, nicht Interrupt.
 //cmd.Process.Signal(os.Interrupt)
 //cmd_peter.Process.Signal(os.Interrupt)
 //cmd.Process.Signal(os.Kill)
 //cmd_peter.Process.Signal(os.Kill)
-
-  // go io.Copy(os.Stderr, stderr_peter)
-    // I love Go's trivial concurrency :-D
-    
-//p,_:=os.FindProcess(cmd_peter.Process.Pid)
-//p.Kill()
-//fmt.Println(cmd_peter.ProcessState.Pid())
-	//go io.PipeWriter(os.Stdin, stdin_peter)
- 
